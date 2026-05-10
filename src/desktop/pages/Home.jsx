797df29f0b142f475a8453a94fc2015f956e6141
debasish <assens.1234@gmail.com>
2026-05-11 02:25:20 +0530

    dashboard-3

--- src/desktop/pages/Home.jsx
+++ src/desktop/pages/Home.jsx
@@ -42,7 +42,7 @@ function Home() {
       // Team size
       const users = await allUsers();
       console.log(users);
-      if (users?.users) {
+      if (users) {
         setEmployeeCount(users.length);
       }
 
